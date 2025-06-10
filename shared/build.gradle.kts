74f642c8cf6038dbbec73e13dedff9d01dff763e
David <david@cappell.net>
2025-06-10 22:47:49 +0200

    fix: publish shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    `maven-publish`
+}
+
 dependencies {
     api(libs.mcsports.proto.kotlin) {
         exclude("io.grpc")
@@ -15,4 +19,24 @@ dependencies {
         exclude("io.grpc")
     }
     api(libs.bundles.grpc)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "MCSports"
+            url = uri("https://repo.mcsports.club/releases")
+            credentials {
+                username = "deploy"
+                password = System.getenv("REPO_TOKEN") ?: ""
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            val hash = System.getenv("COMMIT_HASH")
+            version = if (hash != null) "${rootProject.version}-$hash" else rootProject.version.toString()
+        }
+    }
 }
