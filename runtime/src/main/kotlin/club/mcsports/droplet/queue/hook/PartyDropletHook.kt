87d09586f90e19a686733a9a09955e80789dab97
jannik <jannik.developer02@gmail.com>
2025-06-16 14:53:46 +0200

    convert to object

--- runtime/src/main/kotlin/club/mcsports/droplet/queue/hook/PartyDropletHook.kt
+++ runtime/src/main/kotlin/club/mcsports/droplet/queue/hook/PartyDropletHook.kt
@@ -3,13 +3,14 @@ package club.mcsports.droplet.queue.hook
 import club.mcsports.droplet.party.api.PartyApi
 import org.apache.logging.log4j.LogManager
 
-class PartyDropletHook {
+object PartyDropletHook {
 
     val api = try {
         PartyApi.createCoroutineApi()
     } catch (_: ClassNotFoundException) {
         LogManager.getLogger(PartyDropletHook::class.java)
             .warn("Failed to load party api: Couldn't find api classes. No party features will be considered")
+        null
     }
 
 }
