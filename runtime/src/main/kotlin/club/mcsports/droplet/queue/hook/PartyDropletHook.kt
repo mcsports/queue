498caacf3a185047fcc142128190231f3b0e4fa0
Jannik <jannik.developer02@gmail.com>
2025-07-20 21:47:40 +0200

    create function to extract players that are in the party of the enqueue player

--- runtime/src/main/kotlin/club/mcsports/droplet/queue/hook/PartyDropletHook.kt
+++ runtime/src/main/kotlin/club/mcsports/droplet/queue/hook/PartyDropletHook.kt
@@ -1,9 +1,18 @@
 package club.mcsports.droplet.queue.hook
 
+import app.simplecloud.plugin.api.shared.extension.text
 import club.mcsports.droplet.party.api.PartyApi
+import club.mcsports.droplet.party.shared.extension.getMember
+import club.mcsports.droplet.queue.Color
+import club.mcsports.droplet.queue.extension.fetchPlayer
+import club.mcsports.droplet.queue.extension.log
+import com.mcsports.party.v1.PartyRole
+import io.grpc.Status
+import io.grpc.StatusException
 import org.apache.logging.log4j.LogManager
+import java.util.*
 
-object PartyDropletHook {
+class PartyDropletHook(authSecret: String) {
     private val logger = LogManager.getLogger(PartyDropletHook::class.java)
 
     val api = try {
@@ -17,6 +26,26 @@ object PartyDropletHook {
             logger.warn("Failed to load party api: Couldn't find api classes. No party features will be considered")
         } else logger.error(exception.stackTraceToString())
         null
+    }
+
+    suspend fun queueWithParty(enqueueUuid: UUID): Set<String> {
+        val party = try {
+            api?.getData()?.getParty(enqueueUuid)
+        } catch (exception: StatusException) {
+            if (exception.status.code == Status.Code.NOT_FOUND) return setOf()
+
+            throw exception.status.log(logger).asRuntimeException()
+        }
+        val enqueuePlayer = enqueueUuid.fetchPlayer()
+        val enqueueMember = party?.getMember(enqueueUuid) ?: return setOf()
+
+        if (enqueueMember.role != PartyRole.OWNER) {
+            enqueuePlayer.sendMessage(text("${Color.RED} You must be the party owner in order to enqueue."))
+            throw Status.PERMISSION_DENIED.withDescription("Failed to enqueue: ${enqueueMember.name} isn't the party owner")
+                .log(logger).asRuntimeException()
+        }
+
+        return party.membersList.map { it.uuid }.toSet()
     }
 
 }
--- runtime/src/main/kotlin/club/mcsports/droplet/queue/service/QueueInteractionService.kt
+++ runtime/src/main/kotlin/club/mcsports/droplet/queue/service/QueueInteractionService.kt
@@ -3,62 +3,33 @@ package club.mcsports.droplet.queue.service
 import app.simplecloud.plugin.api.shared.extension.text
 import club.mcsports.droplet.queue.Color
 import club.mcsports.droplet.queue.QueueRepository
+import club.mcsports.droplet.queue.extension.asUuid
 import club.mcsports.droplet.queue.extension.fetchPlayer
 import club.mcsports.droplet.queue.extension.log
 import club.mcsports.droplet.queue.hook.PartyDropletHook
-import com.mcsports.party.v1.PartyRole
-import com.mcsports.queue.v1.DequeueRequest
-import com.mcsports.queue.v1.DequeueResponse
-import com.mcsports.queue.v1.EnqueueRequest
-import com.mcsports.queue.v1.EnqueueResponse
-import com.mcsports.queue.v1.QueueInteractionGrpcKt
-import com.mcsports.queue.v1.dequeueResponse
-import com.mcsports.queue.v1.enqueueResponse
+import com.mcsports.queue.v1.*
 import io.grpc.Status
 import org.apache.logging.log4j.LogManager
-import java.util.UUID
+import java.util.*
 
 class QueueInteractionService(
     private val queues: QueueRepository,
+    private val partyDropletHook: PartyDropletHook,
 ) : QueueInteractionGrpcKt.QueueInteractionCoroutineImplBase() {
     private val logger = LogManager.getLogger(QueueInteractionService::class.java)
 
     override suspend fun enqueue(request: EnqueueRequest): EnqueueResponse {
-        try {
-            val tempPlayerIds = request.playerIdsList.toMutableSet()
+        val tempPlayerIds = request.playerIdsList.toMutableSet()
+        if (request.playerIdsList.size == 1) tempPlayerIds.addAll(partyDropletHook.queueWithParty(request.playerIdsList.first().asUuid()))
 
-            if(request.playerIdsList.size == 1) {
-                PartyDropletHook.api?.let { api ->
-                    val enqueueUuid = UUID.fromString(request.playerIdsList.first())
-                    val party = api.getData().getParty(enqueueUuid)
-                    val enqueuePlayer = enqueueUuid.fetchPlayer()
+        val queue = queues.enqueue(request.queueName, tempPlayerIds.map { UUID.fromString(it) })
+        tempPlayerIds.forEach { uuid ->
+            uuid.fetchPlayer()
+                .sendMessage(text("<white>You ${Color.GREEN}successfully</color> enqueued for ${request.queueName}."))
+        }
 
-                    val enqueueMember = party.membersList.firstOrNull { it.uuid == enqueueUuid.toString() } ?: run {
-                        enqueuePlayer.sendMessage(text("${Color.RED} Failed to fetch your party member data. Please call an administrator about this."))
-                        throw Status.DATA_LOSS.withDescription("Failed to enqueue: Error while fetching party member").log(logger).asRuntimeException()
-                    }
-
-                    if(enqueueMember.role != PartyRole.OWNER) {
-                        enqueuePlayer.sendMessage(text("${Color.RED} You must be the party owner in order to enqueue."))
-                        throw Status.PERMISSION_DENIED.withDescription("Failed to enqueue: ${enqueueMember.name} isn't the party owner")
-                            .log(logger).asRuntimeException()
-                    }
-
-                    tempPlayerIds.addAll(party.membersList.map { it.uuid })
-                }
-            }
-
-            val queue = queues.enqueue(request.queueName, tempPlayerIds.map { UUID.fromString(it) })
-            tempPlayerIds.forEach { uuid ->
-                uuid.fetchPlayer().sendMessage(text("<white>You ${Color.GREEN}successfully</color> enqueued for ${request.queueName}."))
-            }
-
-            return enqueueResponse {
-                queueId = queue.id.toString()
-            }
-        } catch (exception: Exception) {
-//            logger.warn(exception.stackTraceToString())
-            throw exception
+        return enqueueResponse {
+            queueId = queue.id.toString()
         }
     }
 
